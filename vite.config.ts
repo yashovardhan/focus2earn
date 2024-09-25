8dd7fd3b6e5227a712a0568c86839af09c8b6b26
Yashovardhan Agrawal <21066442+yashovardhan@users.noreply.github.com>
2024-09-25 17:47:44 +0530

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,14 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
+// IMP START - Bundler Issues
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       crypto: "empty-module",
-      assert: "empty-module",
-      http: "empty-module",
-      https: "empty-module",
-      os: "empty-module",
-      url: "empty-module",
-      zlib: "empty-module",
-      stream: "empty-module",
-      _stream_duplex: "empty-module",
-      _stream_passthrough: "empty-module",
-      _stream_readable: "empty-module",
-      _stream_writable: "empty-module",
-      _stream_transform: "empty-module",
     },
   },
   define: {
@@ -27,4 +17,5 @@ export default defineConfig({
   build: {
     target: "esnext",
   },
+  // IMP END - Bundler Issues
 });
